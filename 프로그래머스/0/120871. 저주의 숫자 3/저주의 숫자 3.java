735e0d3b8fc44c15a926c373850ac5610a4dc75e
HyeonwooNa <163725763+HyeonwooNa@users.noreply.github.com>
2024-08-21 19:56:07 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.26 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,16 @@
+class Solution {
+        public static int solution(int n) {
+        int tmp = 0;
+
+        for (int i = 0; i < n; i++) {
+            tmp++;
+
+            while(tmp % 3 == 0 || String.valueOf(tmp).contains("3")) {
+                tmp++;
+            }
+
+        }
+
+        return tmp;
+    }
+}
